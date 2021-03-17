1fba90d08ec6bcd2131cfe759ac4844ed401908e
AndreasHNielsen <60094958+AndreasHNielsen@users.noreply.github.com>
2021-03-17 15:56:19 +0100

    0.1

--- classifier.py
+++ classifier.py
@@ -1,1 +1,26 @@
 # classifier
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
+training_data = datasets.FashionMNIST(
+    root= "data",
+    train = True,
+    download = False,
+    transform=ToTensor(),
+)
+
+test_data = datasets.FashionMNIST(
+    root = "data",
+    train = False,
+    download = False,
+    transform=ToTensor()
+)
+
+
+
+
+
