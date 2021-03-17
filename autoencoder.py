c802b18801101003eb17bb6854a21ac088183ed3
Emil Ortvald <eortvald@gmail.com>
2021-03-17 17:28:56 +0100

    updated autoencoder.py with np array

--- autoencoder.py
+++ autoencoder.py
@@ -1,7 +1,21 @@
 # autoencoder
 
 import numpy as np
+import torch, torchvision
 
-arr = np.array([1,2,3,4,5])
 
-print(arr)
+class simpleAE():
+
+    def __init__(self):
+        pass
+
+    def feedforward(self):
+        pass
+
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print('Det virker som det skal , hvis du kan se dette')
+
+if __name__ == "__main__":
+    print(arr)
