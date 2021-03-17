ba8af868d9546212bd8f70d81ecad6ca0247cd75
Emil Ortvald <eortvald@gmail.com>
2021-03-17 17:35:56 +0100

    Added: if __name__ == '__main__':
    to autoencoder.py and main.ipynb

--- autoencoder.py
+++ autoencoder.py
@@ -17,5 +17,9 @@ arr = np.array([1, 2, 3, 4, 5])
 
 print('Det virker som det skal , hvis du kan se dette')
 
+
+
 if __name__ == "__main__":
-    print(arr)
+    # Script in under this statement will only be run when this file is executed
+    # If you import and run this file from another script,
+    # the interpreter will ignore function call made in this statement
