d27cea895e06830d5e7c660abddad9737d178800
Emil Ortvald <eortvald@gmail.com>
2021-04-02 20:02:02 +0200

    Setup placeholder architecture in autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -4,27 +4,42 @@ import torch, torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 
-#The tensors will be moved to CUDA GPU if it is avaialbe,
+# The tensors will be moved to CUDA GPU if available,
 # if not - it will stay on CPU
 torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-#Test
-tensor = torch.rand(3,4)
+# Test
+tensor = torch.rand(3, 4)
 print(f"Shape of tensor: {tensor.shape}")
 print(f"Datatype of tensor: {tensor.dtype}")
 print(f"Device tensor is stored on: {tensor.device}")
 
-
 class VAE(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_: int,):
+        super(VAE, self).__init__()
 
-        #init encoder
+        # Encoder setup
+        self.encode = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1))
+
+        # Decoder setup
+        self.decode = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, 3, stride=2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, 5, stride=3, padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 3, 10, stride=2, padding=1),
+            nn.Tanh()
+        )
 
 
-        #init decoder
-        pass
-    def encode(self,):
+    def encode(self):
         pass
 
     def decode(self):
@@ -34,10 +49,16 @@ class VAE(nn.Module):
         pass
 
     def forward(self):
+
+        # encode out put to mean and var
+
+        # reparametize mean and var and to get Z (code layer)
+
+        #decode z
         pass
 
     def loss_function(self):
-        #ELBO
+        # ELBO
         pass
 
     def sample(self):
@@ -46,11 +67,12 @@ class VAE(nn.Module):
     def generate(self):
         pass
 
-#Tensors to Numpy array
-#Tensor.numpy()
+
+# Tensors to Numpy array
+# Tensor.numpy()
 
 if __name__ == "__main__":
-    prediction = VAE(data)
+    VariationalAE = VAE()
 
     # Script in under this statement will only be run when this file is executed
     # If you import and run this file from another script,
