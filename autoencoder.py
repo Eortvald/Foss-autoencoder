b18955730aafc957c7b4304c1c663d2ba91364e8
Emil Ortvald <eortvald@gmail.com>
2021-04-07 15:18:39 +0200

    running autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import torch, torchvision
 from torch import nn, optim
 from torch.nn import functional as F
-
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-# The tensors will be moved to CUDA GPU if available,
-# if not - it will stay on CPU
-torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# The device will be set to CUDA GPU if available,
+# if not - it will de set to CPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using {device} device')
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -40,6 +40,8 @@ class VAE(nn.Module):
     def __init__(self, z_dim):
         super(VAE, self).__init__()
 
+        self.z_dim = z_dim
+
         # Encoder setup
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=0),
@@ -49,35 +51,33 @@ class VAE(nn.Module):
             nn.ReLU(True),
             nn.MaxPool2d(2, stride=1),
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
-            nn.Flatten(0, -1)
+
         )
 
-        self.z_dim = z_dim
-        self.LinC_mean = nn.Linear(25088, z_dim)
-        self.LinC_var = nn.Linear(25088, z_dim)
-
+        self.LinC_mean = nn.Linear(6272, z_dim)
+        self.LinC_var = nn.Linear(6272, z_dim)
 
         # Decoder setup
-        self.decoder_link = nn.Linear(z_dim, 25088)
+        self.decoder_link = nn.Linear(z_dim, 6272)
 
         self.decoder = nn.Sequential(
-            nn.Unflatten(-1, (4, 128, 7, 7)),
-            nn.ConvTranspose2d(128, 64, 3, stride=2),
+            nn.ConvTranspose2d(128, 64, 2, stride=1, padding=0),
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1),
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=0),
             nn.ReLU(True),
-            nn.ConvTranspose2d(32, 3, 3, stride=2, padding=1),
+            nn.ConvTranspose2d(32, 3, 2, stride=2, padding=1),
             nn.Tanh()
         )
 
     def encode(self, x):
         encode_out = self.encoder(x)
-        print(encode_out.shape)
+        encode_out = encode_out.view(encode_out.size(0), -1)
+        # print(encode_out.shape)
 
         mean = self.LinC_mean(encode_out)
         log_var = self.LinC_var(encode_out)
 
-        print(f'Mean Tensor: {mean.shape} \n Var Tensor: {log_var.shape}')
+        # print(f'Mean Tensor: {mean.shape} \n Var Tensor: {log_var.shape}')
 
         return [mean, log_var]
 
@@ -90,6 +90,10 @@ class VAE(nn.Module):
     def decode(self, Z):
         # Dense layer
         Z = self.decoder_link(Z)
+        # print(f'Z shape : {Z.shape}')
+        Z = Z.view(Z.size(0), 128, 7, 7)
+
+        # print(f'Z shape : {Z.shape}')
 
         # Reshape Linear into Conv ready shape again
         decode_out = self.decoder(Z)
@@ -102,12 +106,11 @@ class VAE(nn.Module):
 
         # reparametize mean and var and to get Z (code layer)
         Z = self.reparameterize(mean, log_var)
-        print(Z.device)
 
         # decode z to get x_hat
         x_hat = self.decode(Z)
 
-        return [x_hat, x, mean, log_var]
+        return [x_hat, mean, log_var]
 
     def grain_from_Zspace(self, num_grain):
         generated_z = torch.randn(num_grain)
@@ -130,7 +133,8 @@ def loss_function(x_hat, x, mean, log_var):
     :return: Cross entropy + KL divergence between the two distributions q and p
     """
     # Cross Entropy
-    BCE = F.binary_cross_entropy(x_hat, x)
+    # print(f'{x_hat}, \n MIN:{torch.min(x_hat)} \n MAX: {torch.max(x_hat)}')
+    BCE = F.mse_loss(x_hat, x)
 
     # KL divergence
     KLD = torch.mean(-0.5 * torch.sum(1 + log_var - mean ** 2 - log_var.exp(), dim=1), dim=0)
@@ -146,14 +150,15 @@ if __name__ == "__main__":
     for epoch in range(num_epochs):
         train_loss = 0.0
         for batch_idx, (img, _) in enumerate(trainloader):
+            optimizer.zero_grad()
             output, mean, log_var = model(img)
             loss = loss_function(output, img, mean, log_var)
-            optimizer.zero_grad()
             loss.backward()
+            train_loss += loss.item()
             optimizer.step()
 
-            to_print = f"epoch:{epoch + 1} of {num_epochs} | Loss: {loss.data[0] / batch_size}"
-
+        to_print = f"epoch:{epoch + 1} of {num_epochs} | Loss: {train_loss / batch_size}"
+        print(to_print)
     torch.save(model.state_dict(), './VAE.pth')
     # Script in under this statement will only be run when this file is executed
     # If you import and run this file from another script,
