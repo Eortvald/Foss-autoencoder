847c8fd386d171a16bc18de155acafbb75106650
AndreasHNielsen <andreas.hornemann@gmail.com>
2021-04-21 16:43:18 +0200

    CNNCifar10 py

--- classifier.py
+++ classifier.py
@@ -1,168 +1,154 @@
-import os
-import pandas as pd
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
-from torchvision.transforms import ToTensor, Lambda
-from torchvision import datasets
-import matplotlib.pyplot as plt
-import numpy as np
-"""
+import os
 
-df = pd.read_csv("Path + filename")
+# Loading and transforming data (Currently transforming the 3 (RGB) channels to a normalized value -1 to 1
+# Consider what the batch_size means in our case of dat
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-with os.scandir("Path til billeder") as entries:
-    for entry in entries:
-        img = np.load(entry)
-        label = df. iloc[entry.split(".")[0]]
+trainset = torchvision.datasets.CIFAR10(root='/home/CIFAR-10 Classifier Using CNN in PyTorch/data/',
+                                        train=True,
+                                        download=True,
+                                        transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=4,
+                                          shuffle=True)
 
-"""
+testset = torchvision.datasets.CIFAR10(root='./data',
+                                       train=False,
+                                       download=True,
+                                       transform=transform)
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=4,
+                                         shuffle=False)
+#The 10 classes, will be changed in our case
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+#Display a random batch of training images
+def convert_to_imshow_format(image):
+    # first convert back to [0,1] range from [-1,1] range
+    image = image / 2 + 0.5
+    image = image.numpy()
+    # convert from CHW to HWC
+    # from 3x32x32 to 32x32x3
+    return image.transpose(1,2,0)
 
-training_data = datasets.CIFAR10(
-    root ="data",
-    train=True,
-    download=False,
-    transform=ToTensor()
-)
-
-test_data = datasets.CIFAR10(
-    root ="data",
-    train=False,
-    download=False,
-    transform=ToTensor()
-)
-trainloader = torch.utils.data.DataLoader(training_data, batch_size=4, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(test_data,batch_size=4, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-def imshow(img):
-    img = img/2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1,2,0)))
-    plt.show()
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-imshow(torchvision.utils.make_grid(images))
+fig, axes = plt.subplots(1, len(images), figsize=(12,2.5))
+for idx, image in enumerate(images):
+    axes[idx].imshow(convert_to_imshow_format(image))
+    axes[idx].set_title(classes[labels[idx]])
+    axes[idx].set_xticks([])
+    axes[idx].set_yticks([])
 
-print (' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net,self).__init__()
-        self.conv1 = nn.Conv2d(3,6,5)
-        self.pool  = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(6,16,5)
-        self.fc1   = nn.Linear(16*5*5,120)
-        self.fc2   = nn.Linear(120,84)
-        self.fc3   = nn.Linear(84,10)
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1,16*5*5)
+        x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
 net = Net()
-
-
-
-
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+model_directory_path = '/home/CIFAR-10 Classifier Using CNN in PyTorch/model/'
+model_path = model_directory_path + 'cifar-10-cnn-model.pt'
 
+if not os.path.exists(model_directory_path):
+    os.makedirs(model_directory_path)
 
-for epoch in range(2):
-    running_loss=0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        optimizer.zero_grad()
+if os.path.isfile(model_path):
+    # load trained model parameters from disk
+    net.load_state_dict(torch.load(model_path))
+    print('Loaded model parameters from disk.')
+else:
+    for epoch in range(2):  # loop over the dataset multiple times
 
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
 
-        running_loss += loss.item()
-        if i % 2000 == 1999:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i+1, running_loss/2000))
-            running_loss = 0.0
-print('Finished Training')
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
-
-
-
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training.')
+    torch.save(net.state_dict(), model_path)
+    print('Saved model parameters to disk.')
 dataiter = iter(testloader)
 images, labels = dataiter.next()
 
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ',' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-
-
-net.load_state_dict(torch.load(PATH))
-
-
-
-outputs= net(images)
-
-
-
-_, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
-
-
-
-correct = 0
-total = 0
+fig, axes = plt.subplots(1, len(images), figsize=(12,2.5))
+for idx, image in enumerate(images):
+    axes[idx].imshow(convert_to_imshow_format(image))
+    axes[idx].set_title(classes[labels[idx]])
+    axes[idx].set_xticks([])
+    axes[idx].set_yticks([])
+outputs = net(images)
+sm = nn.Softmax(dim=1)
+sm_outputs = sm(outputs)
+print(sm_outputs)
+total_correct = 0
+total_images = 0
+confusion_matrix = np.zeros([10,10], int)
 with torch.no_grad():
     for data in testloader:
         images, labels = data
         outputs = net(images)
-        _, predicted = torch.max(outputs.data,1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        _, predicted = torch.max(outputs.data, 1)
+        total_images += labels.size(0)
+        total_correct += (predicted == labels).sum().item()
+        for i, l in enumerate(labels):
+            confusion_matrix[l.item(), predicted[i].item()] += 1
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (100*correct/total))
+model_accuracy = total_correct / total_images * 100
+print('Model accuracy on {0} test images: {1:.2f}%'.format(total_images, model_accuracy))
 
+print('{0:10s} - {1}'.format('Category','Accuracy'))
+for i, r in enumerate(confusion_matrix):
+    print('{0:10s} - {1:.1f}'.format(classes[i], r[i]/np.sum(r)*100))
 
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(4):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' %(
-        classes[i], 100*class_correct[i] / class_total[i]))
-
-
-
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-print(device)
-
-
-
-
+fig, ax = plt.subplots(1,1,figsize=(8,6))
+ax.matshow(confusion_matrix, aspect='auto', vmin=0, vmax=1000, cmap=plt.get_cmap('Blues'))
+plt.ylabel('Actual Category')
+plt.yticks(range(10), classes)
+plt.xlabel('Predicted Category')
+plt.xticks(range(10), classes)
+plt.show()
