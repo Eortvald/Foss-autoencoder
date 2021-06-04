a6d2e5eb4a064de34612aea7740a66d15cdaa597
nullerh <80833969+nullerh@users.noreply.github.com>
2021-06-04 11:54:08 +0200

    Updated to take only all true positives.

--- preprocess/Classifier_labeler.py
+++ preprocess/Classifier_labeler.py
@@ -3,10 +3,8 @@ import pandas as pd
 labels = pd.read_csv('labels_folders_color.csv', low_memory=False)
 id = list()
 for i in range(len(labels)):
-    fc, tc = labels.loc[i][1:7][labels.loc[i][1:7] == False].count(), labels.loc[i][1:7][labels.loc[i][1:7] == True].count()
-    if (fc < 6) or (tc < 0):
+    fc, tc = labels.loc[i][1:8][labels.loc[i][1:8] == False].count(), labels.loc[i][1:8][labels.loc[i][1:8] == True].count()
+    if tc == 0:
         id.append(i)
-    else:
-        print(i)
 cl = labels.drop(id)
-cl.to_csv('Classifier_labels.csv')
+cl.to_csv('Classifier_labels.csv', index = False)
