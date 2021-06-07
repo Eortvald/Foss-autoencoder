f5996b4c9ce99e8206d7a56551b311cdeeb249b8
Peter Hulgaard <peter.f.hulgaard@gmail.com>
2021-06-07 16:54:30 +0200

    Progress in the Dataset class

--- data/MyDataset.py
+++ data/MyDataset.py
@@ -4,24 +4,29 @@ from torch import nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import os
-class MyDataset(torch.utils.Dataset):
-    def __init__(self):
+
+path = 'M:/R&D/Technology access controlled/Projects access controlled/AIFoss/Data/BlobArchive/'
+
+def _load_kernel_file(path):
+    folders = os.listdir(path)
+    data_list = list()
+    for folder in folders:
+        with os.listdir(path + folder) as entries:
+            for entry in entries:
+                data_list.append(folder + entry.name.split(".")[0] + '.npy')
+    return data_list
+
+class MyDataset(Dataset):
+    def __init__(self, data_files):
         self.data_files = data_list
         sorted(self.data_files)
 
-    def __getindex__(self, idx):
-        return np.load(self.data_files[idx])
+    def __getindex__(self, index):
+        return np.load(self.data_files[index])
 
     def __len__(self):
         return len(self.data_files)
 
-path = 'M:/R&D/Technology access controlled/Projects access controlled/AIFoss/Data/BlobArchive/'
-folders = os.listdir(path)
-data_list = list()
-for folder in folders:
-    with os.listdir(path + folder) as entries:
-        for entry in entries:
-            data_list.append(folder + entry.name.split(".")[0] + '.npy')
 
-dset = MyDataset()
+train_set = MyDataset()
 loader = torch.utils.DataLoader(dset, num_workers = 8)
