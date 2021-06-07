58f71542b9f3f099def82585a70a0e093928eb0c
Emil Ortvald <eortvald@gmail.com>
2021-06-07 13:47:13 +0200

    updatetd dataload_collection.py

--- data/dataload_collection.py
+++ data/dataload_collection.py
@@ -5,28 +5,28 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 # PATHS
-
+PATH_10K = 'path'
 
 # Transforms
 Norm_transform = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 #Folder loading
 
-def npy_loader(path):
-    sample = torch.from_numpy(np.load(path))
-    return sample
 
+def dataset_from_collection(PATH:str, transform, batch_size: int, filetype: str='.npy'):
 
-def dataset_from_collection(trainPATH:str,testPATH: str, transform, batch_size: int, filetype: str='.npy'):
+    npy_load = lambda PATH:torch.from_numpy(np.load(PATH,allow_pickle=True))
 
-    train_data = datasets.DatasetFolder(root=trainPATH, loader=npy_loader, extensions=[filetype], transform=transform)
+    train_data = datasets.DatasetFolder(root=PATH+'/train', loader=npy_load, extensions=[filetype], transform=transform)
     train_loader = DataLoader(train_data, batch_size=batch_size,num_workers=2 )
 
-    test_data = datasets.DatasetFolder(root=testPATH, loader=npy_loader, extensions=(ext))
+    test_data = datasets.DatasetFolder(root=PATH+'/test', loader=npy_loader, extensions=(ext))
     test_loader = DataLoader(test_data, batch_size=batch_size, num_workers=2)
 
+    return train_loader, test_loader
+
+dataset_from_collection()
+
 #Transforms
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
 
 #Loadder delegation
