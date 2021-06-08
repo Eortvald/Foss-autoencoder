04f40b03e6bb1c5e2ea2647c34ed8fe0448089b8
Peter Hulgaard <peter.f.hulgaard@gmail.com>
2021-06-08 14:55:15 +0200

    Data set class plus pickle tester

--- data/MyDataset.py
+++ data/MyDataset.py
@@ -12,22 +12,23 @@ def _load_kernel_file(path):
     folders = os.listdir(path)
     data_list = list(str())
     for folder in folders:
+        print(path + folder)
         with os.listdir(path + folder) as entries:
             for entry in entries:
                 data_list.append(folder + entry.name.split(".")[0] + '.npy')
-    return np.load(data_list)[0]
+    return data_list
 
 class MyDataset(torch.utils.Dataset):
-    def __init__(self, data_files):
-        self.data_files = np.sort(data_files)
+    def __init__(self):
+        self.data_files = _load_kernel_file(path)
+        self.data_files = np.sort(self.data_files)
 
     def __getindex__(self, index):
-        return _load_kernel_file(self.data_files[index])
+        return np.load(self.data_files[index])
 
     def __len__(self):
         return len(self.data_files)
 
 
-train_files = np.load(path + '00/' + '5c2f4c564162bb128cfb1a00.npy')
-train_set = MyDataset(data_files = train_files)
-loader = torch.utils.DataLoader(train_set, num_workers = 8)
+dset = MyDataset()
+loader = torch.utils.DataLoader(dset, num_workers = 8)
