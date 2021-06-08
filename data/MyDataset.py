e033e9f3f6a97db1118b4d776883bcc4cb28809c
nullerh <80833969+nullerh@users.noreply.github.com>
2021-06-08 10:11:56 +0200

    Experiment with lazy dataloader

--- data/MyDataset.py
+++ data/MyDataset.py
@@ -2,23 +2,23 @@ import numpy as np
 import torch, torchvision
 from torch import nn
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import os
 
 path = 'M:/R&D/Technology access controlled/Projects access controlled/AIFoss/Data/BlobArchive/'
 
 def _load_kernel_file(path):
     folders = os.listdir(path)
-    data_list = list()
+    data_list = list(str())
     for folder in folders:
         with os.listdir(path + folder) as entries:
             for entry in entries:
                 data_list.append(folder + entry.name.split(".")[0] + '.npy')
     return data_list
 
-class MyDataset(Dataset):
-    def __init__(self, data_files):
-        self.data_files = data_list
+class MyDataset(torch.utils.Dataset):
+    def __init__(self):
+        self.data_files = _load_kernel_file(path)
         sorted(self.data_files)
 
     def __getindex__(self, index):
@@ -29,4 +29,4 @@ class MyDataset(Dataset):
 
 
 train_set = MyDataset()
-loader = torch.utils.DataLoader(dset, num_workers = 8)
+loader = torch.utils.DataLoader(train_set, num_workers = 8)
