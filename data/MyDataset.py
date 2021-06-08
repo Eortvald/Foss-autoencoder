5e8f961133ec3c863ef4fc79878bcdbeac3b0894
Peter Hulgaard <peter.f.hulgaard@gmail.com>
2021-06-08 11:28:59 +0200

    Experimentation with huge data loading functionality

--- data/MyDataset.py
+++ data/MyDataset.py
@@ -2,7 +2,8 @@ import numpy as np
 import torch, torchvision
 from torch import nn
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
+from torch.utils import Dataset
 import os
 
 path = 'M:/R&D/Technology access controlled/Projects access controlled/AIFoss/Data/BlobArchive/'
@@ -14,19 +15,19 @@ def _load_kernel_file(path):
         with os.listdir(path + folder) as entries:
             for entry in entries:
                 data_list.append(folder + entry.name.split(".")[0] + '.npy')
-    return data_list
+    return np.load(data_list)[0]
 
 class MyDataset(torch.utils.Dataset):
-    def __init__(self):
-        self.data_files = _load_kernel_file(path)
-        sorted(self.data_files)
+    def __init__(self, data_files):
+        self.data_files = np.sort(data_files)
 
     def __getindex__(self, index):
-        return np.load(self.data_files[index])
+        return _load_kernel_file(self.data_files[index])
 
     def __len__(self):
         return len(self.data_files)
 
 
-train_set = MyDataset()
+train_files = np.load(path + '00/' + '5c2f4c564162bb128cfb1a00.npy')
+train_set = MyDataset(data_files = train_files)
 loader = torch.utils.DataLoader(train_set, num_workers = 8)
