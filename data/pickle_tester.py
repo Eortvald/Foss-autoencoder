de4fa89c4cfa3b0e9143d9818604f0c51adcf72b
Peter Hulgaard <peter.f.hulgaard@gmail.com>
2021-06-09 16:35:28 +0200

    Updated the pickle length height script

--- data/pickle_tester.py
+++ data/pickle_tester.py
@@ -26,8 +26,11 @@ with os.scandir(path + '2018 samples/') as entries:
         for image in pic:
             heights.append(int(float(image['attributes']['Length'])))
             widths.append(int(float(image['attributes']['Width'])))
-        upper = max(heights)
-        plt.hist([widths, heights], bins = 70, label = ['widths', 'heights'])
-        plt.legend(loc='upper right')
-        plt.xticks((np.linspace(upper % 35, upper, upper//35)).astype(int))
-        plt.savefig('C:/Users/Ext1306/PycharmProjects/Foss-autoencoder/plots/preprocess-plots/sizedist')
+
+upper = max(heights)
+plt.hist([widths, heights], bins = 70, label = ['widths', 'heights'])
+plt.legend(loc='upper right')
+plt.xticks((np.linspace(upper % 35, upper, upper//35)).astype(int))
+plt.savefig('C:/Users/Ext1306/PycharmProjects/Foss-autoencoder/plots/preprocess-plots/sizedist')
+np.save('C:/Users/Ext1306/PycharmProjects/Foss-autoencoder/preprocess/pickle_width.npy', widths)
+np.save('C:/Users/Ext1306/PycharmProjects/Foss-autoencoder/preprocess/pickle_height.npy', heights)
