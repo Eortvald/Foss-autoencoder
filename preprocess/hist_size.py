d47670652ab70219c5742fa8ab2c4342ffdb4e67
nullerh <80833969+nullerh@users.noreply.github.com>
2021-06-10 11:25:25 +0200

    Updated the histogram size calculations (reasonable cut-off @ 79x170)

--- preprocess/hist_size.py
+++ preprocess/hist_size.py
@@ -7,8 +7,8 @@ height = np.load('pickle_height.npy')
 #bins = np.linspace(25, 300, 100)
 
 #plt.hist(width, bins, alpha = 0.5, label = 'width')
-print(np.percentile(width, [1, 98]))
-print(np.percentile(height, [1,98]))
+print(np.percentile(width, np.linspace(90,99,10)))
+print(np.percentile(height, np.linspace(90,99, 10)))
 '''
 plt.hist(width, bins = 200, alpha = 0.5, label = 'width')
 plt.legend(loc = 'upper right')
