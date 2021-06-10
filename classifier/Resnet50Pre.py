1906d4f456b655450192bfac0e4a7c2a8b1b3990
Andreas Hornemann Nielsen <andreas.hornemann@gmail.com>
2021-06-10 19:17:40 +0200

    0.1 PreTrainedRes50

--- classifier/Resnet50Pre.py
+++ classifier/Resnet50Pre.py
@@ -1,115 +1,179 @@
-import time
-import numpy as np
-import pandas as pd
-import datetime as dt
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import ImageGrid
-from os import listdir, makedirs, getcwd, remove
-from os.path import isfile, join, abspath, exists, isdir, expanduser
-from PIL import Image
+# MyResNet50
+from __future__ import print_function
+from __future__ import division
+from data.dataload_collection import Ktest_loader, Ktrain_loader
 import torch
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
 import torchvision
 from torchvision import models
-from data.dataload_collection import *
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
 
-train_dl = Ktrain_loader
-valid_dl = Ktest_loader
+# Number of classes in the dataset
+num_classes = 7
 
-use_gpu = torch.cuda.is_available()
-resnet = models.resnet50(pretrained=True)
-inputs, labels = next(iter(train_dl))
-if use_gpu:
-    resnet = resnet.cuda()
-    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-else:
-    inputs, labels = Variable(inputs), Variable(labels)
-outputs = resnet(inputs)
-outputs.size()
+# Batch size for training (change depending on how much memory you have)
+batch_size = 8
 
+# Number of epochs to train for
+num_epochs = 15
 
-def train_model(dataloders, model, criterion, optimizer, scheduler, num_epochs=25):
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
+
+def buildResNet50Model(numClasses):
+    # get the stock PyTorch ResNet50 model w/ pretrained set to True
+    model = torchvision.models.resnet50(pretrained=True)
+
+    # freeze all model parameters so we don’t backprop through them during training (except the FC layer that will be replaced)
+    for param in model.parameters():
+        param.requires_grad = False
+    # end for
+
+    model.conv1 = nn.Conv2d(8, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+
+    # the last (fully connected) layer per the number of classes
+    # first, get/save the current number of input features to the fc layer
+    numFcInputs = model.fc.in_features
+    # now replace the fc layer with minor changes, and using our number of classes
+    model.fc = nn.Sequential(nn.Linear(numFcInputs, 256),
+                             nn.ReLU(),
+                             nn.Dropout(0.2),
+                             nn.Linear(256, numClasses))
+
+    return model
+# end function
+def train_model(model, dataloaders, criterion, optimizer, num_epochs, is_inception=False):
     since = time.time()
-    use_gpu = torch.cuda.is_available()
-    best_model_wts = model.state_dict()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-    dataset_sizes = {'train': len(dataloders['train'].dataset),
-                     'valid': len(dataloders['valid'].dataset)}
 
     for epoch in range(num_epochs):
-        for phase in ['train', 'valid']:
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
             if phase == 'train':
-                scheduler.step()
-                model.train(True)
+                model.train()  # Set model to training mode
             else:
-                model.train(False)
+                model.eval()  # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
 
-            for inputs, labels in dataloders[phase]:
-                if use_gpu:
-                    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
+                # zero the parameter gradients
                 optimizer.zero_grad()
 
-                outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4 * loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
 
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
+                    _, preds = torch.max(outputs, 1)
 
-                running_loss += loss.data[0]
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
-            if phase == 'train':
-                train_epoch_loss = running_loss / dataset_sizes[phase]
-                train_epoch_acc = running_corrects / dataset_sizes[phase]
-            else:
-                valid_epoch_loss = running_loss / dataset_sizes[phase]
-                valid_epoch_acc = running_corrects / dataset_sizes[phase]
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
-            if phase == 'valid' and valid_epoch_acc > best_acc:
-                best_acc = valid_epoch_acc
-                best_model_wts = model.state_dict()
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
-        print('Epoch [{}/{}] train loss: {:.4f} acc: {:.4f} '
-              'valid loss: {:.4f} acc: {:.4f}'.format(
-            epoch, num_epochs - 1,
-            train_epoch_loss, train_epoch_acc,
-            valid_epoch_loss, valid_epoch_acc))
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
 
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
 
+    # load best model weights
     model.load_state_dict(best_model_wts)
-    return model
+    return model, val_acc_history
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        model_ft = buildResNet50Model(num_classes)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 180
+
+print("Initializing Datasets and Dataloaders...")
+dataloaders_dict = {'train': Ktrain_loader,
+                    'val': Ktest_loader
+}
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+model_ft = model_ft.to(device)
+
+# Gather the parameters to be optimized/updated in this run. If we are
+#  finetuning we will be updating all parameters. However, if we are
+#  doing feature extract method, we will only update the parameters
+#  that we have just initialized, i.e. the parameters with requires_grad
+#  is True.
+params_to_update = model_ft.parameters()
+print("Params to learn:")
+if feature_extract:
+    params_to_update = []
+    for name,param in model_ft.named_parameters():
+        if param.requires_grad == True:
+            params_to_update.append(param)
+            print("\t",name)
+else:
+    for name,param in model_ft.named_parameters():
+        if param.requires_grad == True:
+            print("\t",name)
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+criterion = nn.CrossEntropyLoss()
+model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs, is_inception=False))
 
 
-resnet = models.resnet50(pretrained=True)
-# freeze all model parameters
-for param in resnet.parameters():
-    param.requires_grad = False
-
-# new final layer with 16 classes
-num_ftrs = resnet.fc.in_features
-resnet.fc = torch.nn.Linear(num_ftrs, 16)
-if use_gpu:
-    resnet = resnet.cuda()
-
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(resnet.fc.parameters(), lr=0.001, momentum=0.9)
-
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-dloaders = {'train': train_dl, 'valid': valid_dl}
-
-start_time = time.time()
-model = train_model(dloaders, resnet, criterion, optimizer, exp_lr_scheduler, num_epochs=2)
-print('Training time: {:10f} minutes'.format((time.time() - start_time) / 60))
-
+model = buildResNet50Model(7)
+model = model.to(device)
