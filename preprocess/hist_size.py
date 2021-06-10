30d98e135a5c3a501b7f24e8df2f14fe37964b99
nullerh <80833969+nullerh@users.noreply.github.com>
2021-06-10 10:40:10 +0200

    Height and width distributions of new barley samples

--- preprocess/hist_size.py
+++ preprocess/hist_size.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-width = np.load('width_sorted.npy')
-height = np.load('heigh_sorted.npy')
+width = np.load('pickle_width.npy')
+height = np.load('pickle_height.npy')
 
 #bins = np.linspace(25, 300, 100)
 
 #plt.hist(width, bins, alpha = 0.5, label = 'width')
-print(np.percentile(width, [10, 98]))
-plt.hist(height, bins = 200, alpha = 0.5, label = 'height')
+print(np.percentile(width, [1, 98]))
+print(np.percentile(height, [1,98]))
+'''
+plt.hist(width, bins = 200, alpha = 0.5, label = 'width')
 plt.legend(loc = 'upper right')
-plt.savefig('height_distribution')
+plt.savefig('width_distribution')
 plt.show()
+'''
