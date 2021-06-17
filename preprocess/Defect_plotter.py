133a74dc9eac384e97b3b382c928e5a5d40227c6
Peter Hulgaard <peter.f.hulgaard@gmail.com>
2021-06-17 11:23:35 +0200

    Plot of defects

---
+++ preprocess/Defect_plotter.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import datetime
+import random
+
+def defect_plotter(root_path, labels_path):
+    # Later used for plotting
+    fig, ar = plt.subplots(1, 7, sharey = True, sharex = True, constrained_layout = True)
+
+    # Opening labels
+    df = pd.read_csv(labels_path)
+
+    #Initializing defects
+    defect_list = ['Oat', 'Broken', 'Rye', 'Skinned', 'Cleaved', 'Wheat', 'BarleyGreen']
+
+    for i, defects in enumerate(defect_list):
+        options = df.loc[df[defects] == True]
+        choice = options.iloc[random.randint(0, len(options))]
+        img = np.load(root_path + choice['folder'] + '/' + choice['Names'] + '.npy')
+        ar[i].imshow(np.dstack((img[:,:,4], img[:,:,2], img[:,:,1])))
+        ar[i].set_title(defects)
+        ar[i].set_xlim(0, 70)
+        if defects == 'BarleyGreen':
+            ar[i].set_title('Green')
+    plt.savefig('defects')
+    plt.show()
+defect_plotter('M:/R&D/Technology access controlled/Projects access controlled/AIFoss/Data/BlobArchive/', 'labels_folders_color.csv')
