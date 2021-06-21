53b59873d731dfb6128a5170ae7825ee2854fb7e
ext1308 <eortvald@gmail.com>
2021-06-21 18:35:34 +0200

    added to repo

--- classifier/MLP_Classifier.py
+++ classifier/MLP_Classifier.py
@@ -17,14 +17,13 @@ class ANN(nn.Module):
 
     def __init__(self, n_inputs, hidden_out):
         super(ANN, self).__init__()
-
         self.hidden1 = nn.Linear(n_inputs, hidden_out[0])
         self.af1 = nn.ReLU()
-
         self.hidden2 = nn.Linear(hidden_out[0], hidden_out[1])
         self.af2 = nn.ReLU()
         self.hidden3 = nn.Linear(hidden_out[1], hidden_out[2])
         self.af3 = nn.ReLU()
+        self.out = nn.Linear(hidden_out[2],7)
 
     def forward(self, X):
         X = self.hidden1(X)
@@ -33,6 +32,7 @@ class ANN(nn.Module):
         X = self.af2(X)
         X = self.hidden3(X)
         X = self.af3(X)
+        X = self.out(X)
         return X
 
 
@@ -43,6 +43,8 @@ learningrate = 0.001
 def train_model(traindataloader, model, ENC):
     # Optimizing
     train_loss = 0
+    total = 0
+    correct = 0
     dataset_size = len(traindataloader.dataset)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=learningrate, momentum=0.9)
@@ -53,25 +55,29 @@ def train_model(traindataloader, model, ENC):
         inputs = inputs.to(device)
         label = label.to(device)
         inputs = ENC(inputs).to(device)
-        # print(inputs)
-
         optimizer.zero_grad()
         yhat = model(inputs)
-        # print(f'yhat:{yhat}\n')
-        # print(f'label: {label}')
+
         loss = criterion(yhat, label)
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
-        print(f'{len(inputs)*(i+1)}/{dataset_size}')
+        print(f'{len(inputs) * (i + 1)}/{dataset_size}')
+
+        _, yhat = torch.max(yhat, 1)
+
+        for la, pre in zip(label.detach().cpu().numpy(), yhat.detach().cpu().numpy()):
+
+            if pre == la:
+                correct += 1
+            total += 1
 
     train_loss /= dataset_size
     print(f'Avg train loss: {train_loss}')
+    ACC_train = float(correct) / total
 
-    return train_loss
+    return train_loss, ACC_train
 
-
-# test model
 
 def model_evaluate(testdataloader, model, ENC):
     dataset_size = len(testdataloader.dataset)
@@ -82,22 +88,6 @@ def model_evaluate(testdataloader, model, ENC):
 
     confusion = np.zeros((7, 7))
 
-    label_correct = {1.: 0,
-                     2.: 0,
-                     3.: 0,
-                     4.: 0,
-                     5.: 0,
-                     6.: 0,
-                     7.: 0}
-
-    label_total = {1.: 0,
-                   2.: 0,
-                   3.: 0,
-                   4.: 0,
-                   5.: 0,
-                   6.: 0,
-                   7.: 0}
-
     with torch.no_grad():
         for i, (img, label) in enumerate(testdataloader):
             # Evaluating model on test set
@@ -106,47 +96,29 @@ def model_evaluate(testdataloader, model, ENC):
             inputs = img.to(device)
             inputs = ENC(inputs)
             yhat = model(inputs)
+
             # Purely for print statement
-            print(yhat)
             loss = criterion(yhat, label)
-            print(loss)
+
             test_loss += loss.item()
 
-
             _, yhat = torch.max(yhat, 1)
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print(yhat)
-            for la, pre in zip(label.detach().cpu().numpy(), yhat.detach().cpu().numpy()):
-                print(la)
-                print(pre)
 
-                label_total[la] += 1
+            for la, pre in zip(label.detach().cpu().numpy(), yhat.detach().cpu().numpy()):
+
                 if pre == la:
-                    label_correct[pre] += 1
                     correct += 1
                 total += 1
 
                 confusion[int(la - 1)][int(pre - 1)] += 1
 
-        print(confusion)
-        ACC = 100 * float(correct) / total
-
-        c_acc = [label_correct[i] / label_total[i] * 100 for i in [1., 2., 3., 4., 5., 6., 7.]]
-
-        per_class = {'Oat': c_acc[1],
-                     'Broken': c_acc[2],
-                     'Rye': c_acc[3],
-                     'Wheat': c_acc[4],
-                     'BarleyGreen': c_acc[5],
-                     'Cleaved': c_acc[6],
-                     'Skinned': c_acc[7]}
-
+        ACC = float(correct) / total
         # Accuracy calculation and print
         test_loss /= dataset_size
-        print(f'Avg. test loss {test_loss}  | Accuracy: {ACC} \n Class Accuracy {per_class}')
+        print(f'Avg. test loss {test_loss}  | Accuracy: {100*ACC}')
         print(confusion)
 
-    return test_loss
+    return test_loss, ACC
 
 
 if __name__ == "__main__":
@@ -167,12 +139,12 @@ if __name__ == "__main__":
     DATA_SET = 'validation_blob'
     PATH = PATH_dict[DATA_SET]
 
-    BSIZE = 5
+    BSIZE = 100
     classes = ['Oat', 'Broken', 'Rye', 'Wheat', 'BarleyGreen', 'Cleaved', 'Skinned']
     hidden_out = [16, 12, 10]
     ANN_10Kmodel = ANN(30, hidden_out)
     ANN_10Kmodel = ANN_10Kmodel.to(device)
-    num_epochs = 50
+    num_epochs = 20
     learning_rate = 1e-3
     w_decay = 1e-5
     PIN = True
@@ -194,37 +166,54 @@ if __name__ == "__main__":
 
     train_log = []
     test_log = []
+    acc1_log = []
+    acc2_log = []
 
     start_time = timeit.default_timer()
     for epoch in range(num_epochs):
         print(f'\n\t\t------------------------------Epoch: {epoch + 1}------------------------------')
-        tr_loss = train_model(trainload, ANN_10Kmodel, ENCO)
+        tr_loss, acctrain = train_model(trainload, ANN_10Kmodel, ENCO)
         train_log.append(tr_loss)
+        acc1_log.append(acctrain)
 
         print('\t\t\t>>>>>>>>>>>>>>>>TEST RESULTS<<<<<<<<<<<<<<<<<')
 
-        te_loss = model_evaluate(testload, ANN_10Kmodel, ENCO)
+        te_loss, acctest = model_evaluate(testload, ANN_10Kmodel, ENCO)
         test_log.append(te_loss)
+        acc2_log.append(acctest)
 
-        torch.save(ANN_10Kmodel.state_dict(), f'classifier/model_dicts/ANN_{num_epochs}.pth')
+        torch.save(ANN_10Kmodel.state_dict(), f'model_dicts/ANN_{num_epochs}.pth')
 
     end_time = timeit.default_timer() - start_time
 
     print(end_time)
 
-    torch.save(ANN_10Kmodel.state_dict(), 'classifier/model_dicts/ANN_Big.pth')
+    torch.save(ANN_10Kmodel.state_dict(), 'model_dicts/ANN_Big.pth')
 
     SESSION = str(datetime.now())[5:-10].replace(' ', '_').replace(':', '-')
-    np.savez(f'model_dicts/session_results-{SESSION}', train_log, test_log)
+    np.savez(f'model_dicts/session_loss-{SESSION}', train_log, test_log)
+    np.savez(f'model_dicts/session_acc-{SESSION}', train_log, test_log)
+
+    print(train_log)
+    print(test_log)
 
     figR, ax = plt.subplots()
-    ax.plot(train_log, label='Train')
-    ax.plot(test_log, label='Test')
+    ax.plot(np.array(train_log), label='Train')
+    ax.plot(np.array(test_log), label='Test')
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Loss')
     ax.set_title(f"Train vs Test \t - {DATA_SET} \nBatch size:{BSIZE} | Dataset size:{len(trainload.dataset)}")
     ax.legend()
-    figR.savefig(f"../plots/autoencoder-plots/session_results-{SESSION}.png")
+    figR.savefig(f"../plots/classifier-plots/ANNsession_loss-{SESSION}.png")
+
+    figA, axA = plt.subplots()
+    axA.plot(np.array(acc1_log), label='Train')
+    axA.plot(np.array(acc2_log), label='Test')
+    axA.set_xlabel('Epoch')
+    axA.set_ylabel('Accuracy')
+    axA.set_title(f"Train vs Test \t - {DATA_SET} \nBatch size:{BSIZE} | Dataset size:{len(trainload.dataset)}")
+    axA.legend()
+    figA.savefig(f"../plots/classifier-plots/ANNsession_accuracy-{SESSION}.png")
 
     # https://towardsdatascience.com/pytorch-tabular-multiclass-classification-9f8211a123ab
 
